36984792d8d6fb211f541da1a7e2f709c7c207db
man-singh-dev <mansingh209engineer@gmail.com>
2026-04-26 09:35:22 +0530

    clean frontend

--- migrations/001_init.down.sql
+++
@@ -1,6 +0,0 @@
--- Sab tables drop karo reverse order mein
-DROP TABLE IF EXISTS scan_results;
-DROP TABLE IF EXISTS frame_embeddings;
-DROP TABLE IF EXISTS frame_phashes;
-DROP TABLE IF EXISTS protected_videos;
-DROP EXTENSION IF EXISTS vector;
